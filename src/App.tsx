18223eab6269bafdef0366e369b6007eb3a38f39
Oscar Johander <ocyj@protonmail.com>
2022-05-12 21:05:14 +0200

    Make game playable.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import Chorusle from "./chorusle/Chorusle";
 import "./App.css";
+import { useState } from "react";
+
+import data from "./choruses.json";
+console.log(data);
+
+const choruses: Array<string> = data.choruses;
+// const choruses = ["one two three four"];
+
+const getRandomChorus = () =>
+  choruses[Math.floor(Math.random() * choruses.length)];
 
 function App() {
-  const answer = "One Two Three Four";
-
+  const [id, setId] = useState(0);
+  const [answer, setAnswer] = useState(getRandomChorus());
   return (
     <>
       <h1 className="app-header">CHORUSLE</h1>
-      <Chorusle answer={answer} numGuessesAllowed={6} />
+      <Chorusle
+        key={id}
+        answer={answer}
+        numGuessesAllowed={6}
+        onRestart={() => {
+          setId((id) => id + 1);
+          setAnswer(getRandomChorus());
+        }}
+      />
     </>
   );
 }
--- src/chorusle/Chorusle.tsx
+++ src/chorusle/Chorusle.tsx
@@ -5,14 +5,18 @@ import GuessList from "./GuessList";
 import InputGuess from "./InputGuess";
 import checkGuess from "./utils";
 import GuessStatus from "./GuessStatusEnum";
+import Modal from "./Modal";
 
 interface ChorusleProps {
   answer: string;
   numGuessesAllowed: number;
+  onRestart: () => void;
 }
 
-function Chorusle({ answer, numGuessesAllowed }: ChorusleProps) {
+function Chorusle({ answer, numGuessesAllowed, onRestart }: ChorusleProps) {
   const [guesses, setGuesses] = useState(new Array<string>());
+  //const [showModal, setShowModal] = useState(false);
+
   const correctAnswer = answer.toLowerCase();
   const numWordsAnswer = correctAnswer.split(" ").length;
 
@@ -20,28 +24,40 @@ function Chorusle({ answer, numGuessesAllowed }: ChorusleProps) {
     checkGuess(correctAnswer, guess.toLowerCase())
   );
 
-  let chorusleBody;
+  let gameStatus = undefined;
+  let modalHeader = "";
 
   if (
     statuses.length > 0 &&
     _.every(_.last(statuses), (status) => status === GuessStatus.Correct)
   ) {
-    chorusleBody = <div style={{ color: "white" }}>U did good!</div>;
+    gameStatus = "won";
+    modalHeader = "Good job!";
+    //setShowModal(true);
   } else if (guesses.length === numGuessesAllowed) {
-    chorusleBody = <div style={{ color: "white" }}>Game Over</div>;
-  } else {
-    chorusleBody = (
-      <>
-        <GuessList
-          numGuessesAllowed={numGuessesAllowed}
-          numWordsAnswer={numWordsAnswer}
-          guesses={guesses}
-          statuses={statuses}
-        />
-        <InputGuess onGuess={(guess) => setGuesses((g) => [...g, guess])} />
-      </>
-    );
+    gameStatus = "lost";
+    modalHeader = "Game over";
+    //setShowModal(true);
   }
+
+  let chorusleBody = (
+    <>
+      <GuessList
+        numGuessesAllowed={numGuessesAllowed}
+        numWordsAnswer={numWordsAnswer}
+        guesses={guesses}
+        statuses={statuses}
+      />
+      <InputGuess onGuess={(guess) => setGuesses((g) => [...g, guess])} />
+      <Modal
+        show={gameStatus !== undefined}
+        header={modalHeader}
+        onClose={onRestart}
+      >
+        !
+      </Modal>
+    </>
+  );
 
   return <>{chorusleBody}</>;
 }
